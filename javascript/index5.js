44d6e79cc56a620cb99007d6b17558dea7466f19
JeffenFay <jeffen.fay@gmail.com>
2018-10-09 09:52:43 +0200

    Problem solved in Regex

--- javascript/index5.js
+++ javascript/index5.js
@@ -1,11 +1,11 @@
 //Fonction permettant le calcul et renvoi du résultat
 function calculus(primeNumber, secNumber){
-  //Math.trunc() prend la partie entière du nombre décimal qui a été converti du texte aux nombres via parseInt()
+  //Math.trunc() prend la partie entière du nombre décimal qui a été converti du texte aux nombres via parseFloat()
   primeNumber = Math.trunc(parseFloat(primeNumber));
   secNumber = parseFloat(secNumber);
   //Multiplication
   product = primeNumber*secNumber;
-  //renvoi de la multiplication
+  //renvoi du résultat de la multiplication
   return product;
 }
 //Contrôle des données et affichage via la fonction Calculus()
@@ -14,12 +14,12 @@ function displayResult(){
   var firstNumber = document.getElementById('firstNumber').value;
   var secondNumber = document.getElementById('secondNumber').value;
   // Déclaration de constantes définissant la Regex pour [le nom et le prénom] et la ville à part
-  const regexFirstlast =  /^[0-9,\.]+$/;
+  const regexFirstScnd =  /^[-+]?[0-9]+(\.[0-9]+)?([eE][-+]?[0-9]+)?$/;
   // Condition gérant les erreurs de saisie
-  if(!regexFirstlast.test(firstNumber)||firstNumber==null){
+  if(!regexFirstScnd.test(firstNumber)||firstNumber==null){
     //PAS BON : Premier nombre
     alert("Erreur de saisie : Peu importe ce que vous avez mis mais "+firstNumber+", c'est pas bon !");
-  }else if((!regexFirstlast.test(secondNumber)||secondNumber==null)){
+  }else if((!regexFirstScnd.test(secondNumber)||secondNumber==null)){
     //PAS BON : Deuxième nombre
     alert("Erreur de saisie : Peu importe ce que vous avez mis mais "+secondNumber+", c'est pas bon !");
   }else{
